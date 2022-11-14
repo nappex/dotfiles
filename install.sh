1abfc91670ec295300d97918f4e5650ec2af1181
d.stroch <d.stroch@gmail.com>
2022-11-14 20:57:10 +0100

    Add symlinks for all dofiles

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 # Symlinks
 
-ln -s $HOME/dotfiles/.vim $HOME/.vim
-ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
-ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+ln -s $HOME/dotfiles/.vim $HOME/.vim || echo "$HOME/.vim already exist"
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc || echo "$HOME/.zshrc already exist"
+ln -s $HOME/dotfiles/.bashrc $HOME/.bashrc || echo "$HOME/.bashrc already exist"
+ln -s $HOME/dotfiles/.aliasrc $HOME/.aliasrc || echo "$HOME/.aliasrc already exist"
+ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig || echo "$HOME/.gitconfig already exist"
+ln -s $HOME/dotfiles/.config/git/ignore $HOME/.config/git/ignore || echo "$HOME/.config/git/ignore already exist"
