36c7d63956a1ec83ac6dcc774f3bae7f59ab0a6c
d.stroch <d.stroch@gmail.com>
2022-11-16 22:29:26 +0100

    Create symlinks with for loop
        - new var as array with dotfile names
        - define help func to ask yes or no
        - dont overwirte existing dotfile without ask for it

--- install.sh
+++ install.sh
@@ -1,8 +1,41 @@
-# Symlinks
 
-ln -s $HOME/dotfiles/.vim $HOME/.vim
-ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
-ln -s $HOME/dotfiles/.bashrc $HOME/.bashrc
-ln -s $HOME/dotfiles/.aliasrc $HOME/.aliasrc
-ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
-ln -s $HOME/dotfiles/.config/git/ignore $HOME/.config/git/ignore
+Y_or_N() {
+    printf "%s (y/N): " $1
+    read answer
+    if [ "$answer" != "${answer#[Yy]}" ]
+    then
+        return 0;
+    else
+        return 1;
+    fi
+}
+
+DOTFILES=(
+    ".vim"
+    ".zshrc"
+    ".bashrc"
+    ".aliasrc"
+    ".gitconfig"
+    ".config/git/ignore"
+)
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+
+# Create symlinks
+
+for dotfile in "${DOTFILES[@]}"; do
+    dst=$HOME/$dotfile
+    if [ -f $dst ]
+    then
+        # Overwrite existing dotfile to symlink?
+        # overwriting is done by option -f as force
+        echo "$dst already exist!"
+        Y_or_N "Do you want to overwrite" \
+            && ln -sf $SCRIPTPATH/$dotfile $dst \
+            && echo "$dst was overwrited as symlink."
+    else
+        ln -s $SCRIPTPATH/$dotfile $dst
+        echo "New symlink $dst was created."
+    fi
+done
