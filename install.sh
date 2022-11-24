382204c4826fc1aea7b0a2f7d691f559efe02765
d.stroch <d.stroch@gmail.com>
2022-11-24 09:53:48 +0100

    Symlink git ignore from new location

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+CONFIG_HOME=$HOME/.config
+
 Y_or_N() {
     printf "%s (y/N): " $1
     read answer
@@ -17,7 +19,6 @@ DOTFILES=(
     ".bashrc"
     ".aliasrc"
     ".gitconfig"
-    ".config/git/ignore"
 )
 
 SCRIPT_DIR_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
@@ -45,6 +46,8 @@ source $SCRIPT_DIR_PATH/zsh/.zshenv
 ln -s $SCRIPT_DIR_PATH/zsh/.zshenv $HOME/.zshenv
 ln -s $SCRIPT_DIR_PATH/zsh/.zshrc $ZDOTDIR/.zshrc
 echo ZDOTDIR=$ZDOTDIR
+
+ln -s $SCRIPT_DIR_PATH/git/ignore $CONFIG_HOME/git/ignore
 
 # INSTALL VIM PLUGINS
 $SCRIPT_DIR_PATH/.vim/install_plugins.sh
