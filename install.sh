25bba656b99a8148cda779cd6545cb22d8600c92
dstroch <d.stroch@gmail.com>
2022-11-25 11:01:13 +0100

    Handle symlinks by command ln with options -Fhiv

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 CONFIG_HOME=$HOME/.config
 
 Y_or_N() {
-    printf "%s (y/N): " $1
+    printf "%s (y/N): " "$1"
     read answer
     if [ "$answer" != "${answer#[Yy]}" ]
     then
@@ -20,23 +20,13 @@ DOTFILES=(
 
 SCRIPT_DIR_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-
 # Create symlinks
 
 for dotfile in "${DOTFILES[@]}"; do
     dst=$HOME/$dotfile
-    if [ -f $dst ]
-    then
-        # Overwrite existing dotfile to symlink?
-        # overwriting is done by option -f as force
-        echo "$dst already exist!"
-        Y_or_N "Do you want to overwrite" \
-            && ln -sf $SCRIPT_DIR_PATH/$dotfile $dst \
-            && echo "$dst was overwrited as symlink."
-    else
-        ln -s $SCRIPT_DIR_PATH/$dotfile $dst \
-            && echo "New symlink $dst was created."
-    fi
+    # Overwrite existing dotfile to symlink?
+    # overwriting is done by option -Fh as force
+    ln -sFhiv $SCRIPT_DIR_PATH/$dotfile $dst
 done
 
 HOME_DOTFILES=(
@@ -49,18 +39,9 @@ HOME_DOTFILES=(
 for dotpath in "${HOME_DOTFILES[@]}"; do
     dotfile="$(cut -d '/' -f 2 <<<"$dotpath")"
     dst=$HOME/"$dotfile"
-    if [ -f $dst ]
-    then
-        # Overwrite existing dotfile to symlink?
-        # overwriting is done by option -f as force
-        echo "$dst already exist!"
-        Y_or_N "Do you want to overwrite" \
-            && ln -sf $SCRIPT_DIR_PATH/$dotpath $dst \
-            && echo "$dst was overwrited as symlink."
-    else
-        ln -s $SCRIPT_DIR_PATH/$dotpath $dst \
-            && echo "New symlink $dst was created."
-    fi
+    # Overwrite existing dotfile to symlink?
+    # overwriting is done by option -Fh as force
+    ln -sFhiv $SCRIPT_DIR_PATH/$dotpath $dst
 done
 
 # CONFIG_HOME/zsh = ZDOTDIR in .zshenv, dont forget any change to update .zshenv
@@ -72,19 +53,11 @@ CONFIG_DOTFILES=(
 
 for dotpath in "${CONFIG_DOTFILES[@]}"; do
     dst=$CONFIG_HOME/$dotpath
-    if [ -f $dst ]
-    then
-        # overwriting is done by option -f as force
-        echo "$dst already exist!"
-        Y_or_N "Do you want to overwrite" \
-            && ln -sf $SCRIPT_DIR_PATH/$dotpath $dst \
-            && echo "$dst was overwrited as symlink."
-    else
-        ln -s $SCRIPT_DIR_PATH/$dotpath $dst \
-            && echo "New symlink $dst was created."
-    fi
+    # overwriting is done by option -Fh as force
+    ln -sFhiv $SCRIPT_DIR_PATH/$dotpath $dst
 done
 
 
 # INSTALL VIM PLUGINS
 $SCRIPT_DIR_PATH/.vim/install_plugins.sh
+
