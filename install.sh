9253a81547a657e4cd3ba5dfeca43a03ef04f0e5
dstroch <d.stroch@gmail.com>
2023-02-25 21:38:06 +0100

    Rewrite install script to be more POSIX

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@
 # FUNCTIONS
 Y_or_N() {
     printf "%s (y/N): " "$1"
-    read answer
+    read -r answer
     if [ "$answer" != "${answer#[Yy]}" ]
     then
         return 0;
@@ -20,55 +20,48 @@ Y_or_N() {
 }
 
 # CONSTANTS
-SCRIPT_DIR_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+SCRIPT_DIR_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit; pwd -P )"
 CONFIG_HOME=$HOME/.config
 
 # MAKE SOFT LINKS
-DOTFILES=(
-    "vim"
-    "profile"
-    "bashrc"
-    "aliasrc"
-    "zsh/zshenv"
-    "git/gitconfig"
-)
+set -- vim profile bashrc aliasrc zsh/zshenv git/gitconfig
 
-for path in "${DOTFILES[@]}"; do
+for path in "$@"
+do
     # get filename from path
     dotfile=$(basename "$path")
     target=$HOME/."$dotfile"
 
-    if [ -e $target ] || [ -h $target ]
+    if [ -e "$target" ] || [ -h "$target" ]
     then
-       Y_or_N "$target exists, overwrite it?" \
-           && ln -sfn $SCRIPT_DIR_PATH/$path $target \
-           && echo "Soft link $target created successfully"
+        Y_or_N "$target exists, overwrite it?" \
+        && rm "$target" \
+        && ln -s "$SCRIPT_DIR_PATH"/"$path" "$target" \
+        && echo "Soft link $target created successfully"
     else
-        ln -s $SCRIPT_DIR_PATH/$path $target \
-            && echo "Soft link $target created successfully"
+        ln -s "$SCRIPT_DIR_PATH"/"$path" "$target" \
+        && echo "Soft link $target created successfully"
     fi
 done
 
 # CONFIG_HOME/zsh = ZDOTDIR in .zshenv, dont forget any change to update .zshenv
+set -- zsh/.zshrc zsh/.zprofile
 
-CONFIG_DOTFILES=(
-    "zsh/.zshrc"
-    "zsh/.zprofile"
-)
-
-for path in "${CONFIG_DOTFILES[@]}"; do
+for path in "$@"
+do
     # get dirname and make path to ~/.config/<dirname>
     target_dirpath=$CONFIG_HOME/"$(dirname "$path")"
-    target=$CONFIG_HOME/$path
+    target=$CONFIG_HOME/"$path"
 
-    if [ -e $target ] || [ -h $target ]
+    if [ -e "$target" ] || [ -h "$target" ]
     then
-       Y_or_N "$target exists, overwrite it?" \
-       && ln -sfn $SCRIPT_DIR_PATH/$path $target \
-       && echo "Soft link to $target created successfully"
+        Y_or_N "$target exists, overwrite it?" \
+        && rm "$target" \
+        && ln -s "$SCRIPT_DIR_PATH"/"$path" "$target" \
+        && echo "Soft link to $target created successfully"
     else
         mkdir -p "$target_dirpath" \
-        && ln -s $SCRIPT_DIR_PATH/$path $target \
+        && ln -s "$SCRIPT_DIR_PATH"/"$path" "$target" \
         && echo "Soft link to $target created successfully"
     fi
 done
@@ -77,9 +70,8 @@ done
 # ignore file is platform specific the base is copied rest must be fill by platform
 source=$SCRIPT_DIR_PATH/git/ignore
 target=$CONFIG_HOME/git/ignore
-if [ -e $target ] || [ -h $target ]
+if [ -e "$target" ] || [ -h "$target" ]
 then
-# cp: command not found
     Y_or_N "$target exists, overwrite it?" \
     && rm "$target" \
     && cp "$source" "$target" \
@@ -90,5 +82,5 @@ else
 fi
 
 # INSTALL VIM PLUGINS
-$SCRIPT_DIR_PATH/vim/install_plugins.sh
+"$SCRIPT_DIR_PATH"/vim/install_plugins.sh
 
