8f384d9e5eb0978c51902a6863e427a7d3573849
dstroch <d.stroch@gmail.com>
2023-04-25 07:32:09 +0200

    Add swapfile false and fix opt to only 'o'

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,12 @@
-vim.opt.relativenumber = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.mouse = a
+vim.o.relativenumber = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.swapfile = false
+vim.o.mouse = a
 
 -- termguicolors is false by default, but it is good to know the option exist
 -- if you set true it enable 24-bit RGB color in the TUI
-vim.opt.termguicolors = false
+vim.o.termguicolors = false
 vim.cmd.colorscheme('slate')
 
 require("keymaps")
