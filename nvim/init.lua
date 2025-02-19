e6a20257a6b12088c37489a4b95dfb8f0d910e56
dstroch <d.stroch@gmail.com>
2025-02-19 08:31:08 +0100

    Split order of commands

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,8 +4,8 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.swapfile = false
 vim.o.mouse = a
-vim.o.list = true
 vim.o.listchars = 'tab:» ,lead:•,trail:•,nbsp:␣'
+vim.o.list = true
 
 -- termguicolors is false by default, but it is good to know the option exist
 -- if you set true it enable 24-bit RGB color in the TUI
