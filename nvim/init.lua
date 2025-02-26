36db0e116da85f6fb242cf5ea2b6714ca85caa94
dstroch <d.stroch@gmail.com>
2025-02-26 11:16:40 +0100

    Remove duplicate setting

--- nvim/init.lua
+++ nvim/init.lua
@@ -26,9 +26,6 @@ vim.opt.scrolloff = 10
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
 
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
 -- termguicolors is false by default, but it is good to know the option exist
 -- if you set true it enable 24-bit RGB color in the TUI
 vim.o.termguicolors = false
