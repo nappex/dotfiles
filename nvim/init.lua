3c8d6d8eb9b5bffbe3165a6f6c950c7c7c5219ff
dstroch <d.stroch@gmail.com>
2025-03-01 18:11:56 +0100

    Fix .o to .opt

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,7 +28,7 @@ vim.opt.inccommand = 'split'
 
 -- termguicolors is false by default, but it is good to know the option exist
 -- if you set true it enable 24-bit RGB color in the TUI
-vim.o.termguicolors = false
+vim.opt.termguicolors = false
 vim.cmd.colorscheme('slate')
 
 require("keymaps")
